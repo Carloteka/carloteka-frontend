5c6a3e0448900f7a973e2165a391f95f3bf0ea6b
bbbozhena <bbbozhenka@gmail.com>
2023-08-31 14:46:04 +0200

    feat: footer test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import {Footer} from "./components/footer"
 
 function App() {
-  return <div className="App">Hello!</div>;
+  return <div className="App"><Footer/> </div>;
 }
 
 export default App;
