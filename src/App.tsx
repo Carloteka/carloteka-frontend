2ecfc4c0d919204b56da348026f9ffd20c492ca7
bbbozhena <bbbozhenka@gmail.com>
2023-09-04 00:19:24 +0100

    feat: add popular goods block

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { Greeting } from "./components/greeting";
 import { CategoryCard } from "./components/category-card/CategoryCard";
 import { Footer } from "./components/footer";
 import { NewGoods } from "./components/newGoods";
+import { PopularGoods } from "./components/popularGoods";
+
 function App() {
   return (
     <div className="App">
@@ -12,6 +14,7 @@ function App() {
       <CategoryCard order={0} />
       <CategoryCard order={1} />
       <CategoryCard order={0} />
+      <PopularGoods />
       <NewGoods />
       <Footer />
     </div>
