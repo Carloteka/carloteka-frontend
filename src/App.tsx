b9daa954a0355d99e15371ad088b7691211166db
Nazarii Sova <madrigalamar@gmail.com>
2023-09-16 12:53:10 +0300

    add layout for card of category

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
-import { Header } from './components/header';
-import { Greeting } from './components/greeting';
-import { CategoryCard} from './components/category-card/CategoryCard';
+
+import { Header } from './components/header'
+import { Greeting } from './components/greeting'
+import { CategoryCard } from './components/category-card/CategoryCard'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Greeting />
-      <CategoryCard/>
+      <CategoryCard order={0} />
+      <CategoryCard order={1} />
+      <CategoryCard order={0} />
     </div>
   )
 }
--- src/components/category-card/CategoryCard.tsx
+++ src/components/category-card/CategoryCard.tsx
@@ -1,12 +1,37 @@
 import React from 'react'
 import {
-    CategoryCardConatiner,
+  CategoryCardContainer,
+  ImgContainer,
+  TextContainer,
+  Title,
+  Description,
 } from './CategoryCard.styled'
+type Props = { order: number }
 
-export const CategoryCard = () => {
-    return (
-        <CategoryCardConatiner>
-///
-        </CategoryCardConatiner>
-    )
+export const CategoryCard = ({ order }: Props) => {
+  return (
+    <CategoryCardContainer>
+      <ImgContainer order={order}>
+        <button className='btn_left'>
+          <img src="img/left.png" alt="left" />
+        </button>
+        <img src={`img/chess.png`} alt="img першої категорії" />
+        <button className='btn_right'>
+          <img src="img/right.png" alt="right" />
+        </button>
+      </ImgContainer>
+      <TextContainer>
+        <Title>
+          ШАХИ, НАРДИ, ШАШКИ
+          <div className="title_line"></div>
+        </Title>
+        <Description>
+          Бездоганні шахові фігури, різні розміри, види та вподобання.
+        </Description>
+        <button className="card_btn" type="submit">
+          Переглянути
+        </button>
+      </TextContainer>
+    </CategoryCardContainer>
+  )
 }
