1e72a0cc2498c185ecbba7df2ad347788b0d63f1
bbbozhena <bbbozhenka@gmail.com>
2023-09-21 13:46:40 +0200

    add carousel for new goods

--- src/components/newGoods/newGoods.styled.tsx
+++ src/components/newGoods/newGoods.styled.tsx
@@ -8,7 +8,6 @@ export const MainGoodsBlock = styled.div`
 export const GoodsItemsBlock = styled.div`
   display: flex;
   flex-direction: column;
-  margin: 100px 0px;
   h1 {
     margin-left: 10px;
     color: #101010;
@@ -17,13 +16,39 @@ export const GoodsItemsBlock = styled.div`
     font-weight: 600px;
     margin-top: 43px;
   }
+  button {
+    background-color: #f2f0ec;
+    border: none;
+    width: 40px;
+    height: 60px;
+    margin: 0px 25px;
+    cursor: pointer;
+    img {
+      height: 15px;
+      height: 15px;
+    }
+  }
 `;
 export const GoodsCards = styled.div`
   display: flex;
-  justify-content: space-around;
+  .slider-button-prev {
+    position: absolute;
+    width: 50px;
+    height: 70px;
+    border: none;
+    left: 1%;
+  }
+  .slider-button-next {
+    position: absolute;
+    width: 50px;
+    height: 70px;
+    border: none;
+    right: 1%;
+  }
 `;
 
 export const GoodsCardItem = styled.div`
+  margin: 0px 11px;
   .cardImg {
     background-image: url(img/vaza.png);
     background-color: #dad4c8;
@@ -61,4 +86,14 @@ export const GoodsCardItem = styled.div`
     margin-top: 15px;
     font-weight: 400px;
   }
+`;
+
+export const SliderButton = styled.button`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  background-color: transparent;
+  border: none;
+  font-size: 18px;
+  cursor: pointer;
 `;
